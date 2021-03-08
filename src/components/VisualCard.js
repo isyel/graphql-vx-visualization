702c4a6f558bf520b2bd8f92ce83b7bfbf5d6df0
isyel <isykaal4saviour@gmail.com>
2021-03-08 12:29:49 +0100

    Removed unused props property

--- src/components/VisualCard.js
+++ src/components/VisualCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function VisualCard({ children, props }) {
+function VisualCard({ children }) {
 	return <div className="dark-card md:col-span-1 col-span-2">{children}</div>;
 }
 
